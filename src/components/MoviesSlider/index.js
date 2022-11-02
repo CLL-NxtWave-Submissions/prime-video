39fa9a00d273a8a89fa0ece8f61896a405866538
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 19:39:11 +0000

    Implemented JSX element tree in MoviesSlider component to render the carousel of movie thumbnails

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -1,1 +1,27 @@
-// Write your code here
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import MovieItem from '../MovieItem'
+
+const MoviesSlider = props => {
+  const {sliderMoviesList} = props
+  const sliderProps = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  }
+  return (
+    <Slider {...sliderProps}>
+      {sliderMoviesList.map(sliderMoviesListItem => (
+        <div>
+          <MovieItem itemData={sliderMoviesListItem} />
+        </div>
+      ))}
+    </Slider>
+  )
+}
+
+export default MoviesSlider
