fe8ee778a008a0d5caf782ed3e1bfdbfad46bdd5
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 20:25:23 +0000

    Implemented JSX tree in MovieItem component with imports from reactjs-popup, react-player, react-icon npm packages to render modal popup and video player to play the movie trailer, when clicked on the movie item

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,1 +1,46 @@
-// Write your code here
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player/youtube'
+import {IoMdClose} from 'react-icons/io'
+import 'reactjs-popup/dist/index.css'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {itemData} = props
+  const {thumbnailUrl, videoUrl} = itemData
+
+  const popUpTriggerJSX = (
+    <button type="button" className="movie-item-button">
+      <img
+        className="movie-item-thumbnail-img"
+        src={thumbnailUrl}
+        alt="thumbnail"
+      />
+    </button>
+  )
+
+  return (
+    <li className="movie-item-container">
+      <Popup modal trigger={popUpTriggerJSX} className="popup-content">
+        {close => (
+          <div className="popup-container">
+            <div className="popup-header-container">
+              <button
+                className="popup-close-btn"
+                type="button"
+                onClick={() => close()}
+              >
+                <IoMdClose className="close-btn-icon" />
+              </button>
+            </div>
+            <div className="popup-content-container">
+              <ReactPlayer url={videoUrl} />
+            </div>
+          </div>
+        )}
+      </Popup>
+    </li>
+  )
+}
+
+export default MovieItem
