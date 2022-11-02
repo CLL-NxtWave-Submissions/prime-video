e4f920b3a2dd520efbe0342c6b0e62dbff7d19ff
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 18:03:02 +0000

    Moved the react-slick, slick-carousel package imports from PrimeVideo component to MoviesSlider component

--- src/components/PrimeVideo/index.js
+++ src/components/PrimeVideo/index.js
@@ -1,7 +1,3 @@
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
 import MoviesSlider from '../MoviesSlider'
 
 import './index.css'
