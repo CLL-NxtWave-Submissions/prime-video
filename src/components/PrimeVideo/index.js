1f8d42e7fbaf1ec001adda61bebb3907455a5002
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 17:59:08 +0000

    Implemented the JSX element tree in PrimeVideo component to render the intended UI

--- src/components/PrimeVideo/index.js
+++ src/components/PrimeVideo/index.js
@@ -1,1 +1,48 @@
-// Write your code here
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import MoviesSlider from '../MoviesSlider'
+
+import './index.css'
+
+const PrimeVideo = props => {
+  const {moviesList} = props
+
+  const getActionMoviesList = inputMoviesList =>
+    inputMoviesList.filter(
+      inputMoviesListItem => inputMoviesListItem.categoryId === 'ACTION',
+    )
+
+  const getComedyMoviesList = inputMoviesList =>
+    inputMoviesList.filter(
+      inputMoviesListItem => inputMoviesListItem.categoryId === 'COMEDY',
+    )
+
+  const actionMoviesList = getActionMoviesList(moviesList)
+  const comedyMoviesList = getComedyMoviesList(moviesList)
+
+  return (
+    <div className="prime-video-bg-container">
+      <div className="prime-video-img-container">
+        <img
+          className="prime-video-img"
+          src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+          alt="prime video"
+        />
+      </div>
+      <ul className="prime-video-content-container">
+        <li className="prime-video-category-content-container">
+          <h1 className="prime-video-category-name">Action Movies</h1>
+          <MoviesSlider sliderMoviesList={actionMoviesList} />
+        </li>
+        <li className="prime-video-category-content-container">
+          <h1 className="prime-video-category-name">Comedy Movies</h1>
+          <MoviesSlider sliderMoviesList={comedyMoviesList} />
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default PrimeVideo
